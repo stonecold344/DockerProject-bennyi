39ca0ee0ecaad45a3ec1099cc71018a02b6ec3c8
bennyi <stonecold344@gmail.com>
2024-09-06 19:34:17 +0300

    service-deploy.yaml-commit25

--- init-mongo.sh
+++ init-mongo.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mongo1_container=$(docker ps --filter "name=mongo1" --format "{{.ID}}")
-docker exec $mongo1_container mongosh --eval 'rs.initiate({_id: "rs0", members: [{_id: 0, host: "mongo1:27017"}, {_id: 1, host: "mongo2:27018"}, {_id: 2, host: "mongo3:27019"}]})'
+docker exec $mongo1_container mongosh --eval '
+rs.status().ok === 1 ? print("Replica set already initialized.") : rs.initiate({_id: "rs0", members: [{_id: 0, host: "mongo1:27017"}, {_id: 1, host: "mongo2:27018"}, {_id: 2, host: "mongo3:27019"}]})
+'
