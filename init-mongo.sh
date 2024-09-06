beaa99ae7c500349edd549ff6149ec03c4dca638
bennyi <stonecold344@gmail.com>
2024-09-06 19:45:20 +0300

    service-deploy.yaml-commit&init-mongo

--- init-mongo.sh
+++ init-mongo.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
+
+# Check if MongoDB is up
+echo "Waiting for MongoDB to be available..."
+until nc -z mongo1 27017; do
+  sleep 1
+done
+
+echo "MongoDB is up, initializing replica set..."
+
+# Run the initialization script
 mongo1_container=$(docker ps --filter "name=mongo1" --format "{{.ID}}")
 docker exec $mongo1_container mongosh --eval '
-rs.status().ok === 1 ? print("Replica set already initialized.") : rs.initiate({_id: "rs0", members: [{_id: 0, host: "mongo1:27017"}, {_id: 1, host: "mongo2:27018"}, {_id: 2, host: "mongo3:27019"}]})
+rs.initiate({
+  _id: "rs0",
+  members: [
+    {_id: 0, host: "mongo1:27017"},
+    {_id: 1, host: "mongo2:27018"},
+    {_id: 2, host: "mongo3:27019"}
+  ]
+})
 '
+
+echo "Replica set initialization script executed."
